546d8a0db2e473653f0505210167732eb0047b72
luiizsilverio <luiiz.silverio@gmail.com>
2021-05-13 14:03:19 -0300

    virtualização

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react'
 import { ProductItem } from "./ProductItem"
+import { List, ListRowRenderer, AutoSizer } from 'react-virtualized'
 
 type Product = {
     id: number
@@ -22,16 +23,39 @@ export function SearchResults({ results }: SearchProps) {
         console.log(id)
     }
 
+    const onRowRenderer: ListRowRenderer = ({ index, key, style }) => {
+        return (            
+            <div key={key} style={style}>
+                <ProductItem 
+                    product={ results[index] } 
+                    onAddToFav={onAddToFav}
+                />
+            </div>
+        )
+    }
+
     return (
         <div>
             <h2>Total: {total}</h2>
+            
+            <List 
+                height={300}
+                width={700}
+                rowHeight={30}
+                overscanRowCount={5}
+                rowCount={results.length}
+                rowRenderer={onRowRenderer}
+            />
+        
+            {/* aqui, carregava os 1000 produtos
             {results.map(prod => (
                 <ProductItem key={prod.id} 
                     product={ prod } 
                     onAddToFav={() => onAddToFav(prod.id)}
                 />
-
             ))}
+            */}
+
         </div>
     )
 }
